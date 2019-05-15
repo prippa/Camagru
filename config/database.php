da3bfdea923fe22e7d099723d295cff8ec798ece
prippa <pavelrippa@gmail.com>
2019-05-15 19:30:45 +0300

    update

---
+++ config/database.php
@@ -0,0 +1,5 @@
+<?php
+
+define('DB_DSN', 'mysql:host=localhost;dbname=example01');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'prippa');
---
+++ config/dirs.php
@@ -0,0 +1,4 @@
+<?php
+
+define('ROOT_DIR', realpath(__DIR__ . '/..') . '/');
+define('UPLOADS_DIR', ROOT_DIR . 'uploads/');
