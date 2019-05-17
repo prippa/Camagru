b9202d25c185390744ffead6f3370a2ed9bc10b2
prippa <pavelrippa@gmail.com>
2019-05-17 17:56:37 +0300

    update

--- trash/Posts/index.show.php
+++ trash/Posts/index.show.php
@@ -2,5 +2,5 @@
 <?php foreach ($posts as $post): ?>
     <h2><?= $post['title']; ?></h2>
     <p><?= $post['content']; ?></p>
-    <img src="<?= '/Camagru/trash/Posts/uploads/' . $post['image']; ?>" width="300">
+    <img src="<?= '/Camagru/trash/Posts/uploads/' . $post['image']; ?>" width="500">
 <?php endforeach; ?>
