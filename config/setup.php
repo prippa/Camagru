ddb8cde10e7054f65844877ac7fb6213f5aea7ca
prippa <pavelrippa@gmail.com>
2019-05-25 18:06:10 +0300

    update

--- config/setup.php
+++ config/setup.php
@@ -1,5 +1,17 @@
 <?php
 
 $db_settings = require 'database.php';
+$DROP_IF_EXIST = 'DROP DATABASE IF EXISTS ' . $db_settings['dbname'];
+$CREATE_DB = 'CREATE DATABASE ' . $db_settings['dbname'] . ' CHARACTER SET utf8 COLLATE utf8_general_ci';
+$USE_DB = 'USE ' . $db_settings['dbname'];
 
-$db =
+try {
+    $db = new PDO('mysql:host=' . $db_settings['host'], $db_settings['user'], $db_settings['password']);
+    $db->query($DROP_IF_EXIST);
+    $db->query($CREATE_DB);
+    $db->query($USE_DB);
+    $sql = file_get_contents($db_settings['dbname'] . '.sql');
+    $db->query($sql);
+} catch (PDOException $e) {
+    echo $e . PHP_EOL;
+}
