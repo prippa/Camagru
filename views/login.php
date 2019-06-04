c2766272383f93b3056a0737c570be2b06d0116b
prippa <pavelrippa@gmail.com>
2019-06-04 11:25:39 +0300

    update

--- views/login.php
+++ views/login.php
@@ -5,10 +5,69 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="/template/css/materialize.min.css" rel="stylesheet">
+    <link href="/template/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/template/css/style.css" rel="stylesheet">
     <title>Login</title>
 </head>
 <body>
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-10">
+            <h4>МНЕНИЕ МЕЖДУНАРОДНОЙ ОБЩЕСТВЕННОСТИ</h4>
+            <hr>
+        </div>
+        <div class="col-md align-self-center">
+            <a class="" href="#">Перейти на страницу</a>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-md">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/eWn7ZrzDvtQ?rel=0" allowfullscreen></iframe>
+            </div>
+            <p>ЗА ДРУЖБУ И ВЗАИМОПОМОЩЬ МЕЖДУ ЛЮДЬМИ</p>
+        </div>
+        <div class="col-md">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Oy9kQYbl-8U?rel=0" allowfullscreen></iframe>
+            </div>
+            <p>ВЗАИМОУВАЖЕНИЕ И СОВЕСТЬ - ОСНОВА СОЗИДАТЕЛЬНОГО ОБЩЕСТВА</p>
+        </div>
+        <div class="col-md">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/eWn7ZrzDvtQ?rel=0" allowfullscreen></iframe>
+            </div>
+            <p>ВРЕМЯ ОТКРЫТО ГОВОРИТЬ О ПРАВДЕ И ДОБРЕ</p>
+        </div>
+
+        <div class="w-100"></div>
+
+        <div class="col-md">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Oy9kQYbl-8U?rel=0" allowfullscreen></iframe>
+            </div>
+            <p>ВЫБИРАЙ ДОБРО</p>
+        </div>
+        <div class="col-md">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/eWn7ZrzDvtQ?rel=0" allowfullscreen></iframe>
+            </div>
+            <p>ТЫ СОЗДАЁШЬ ОБЩЕСТВО В КОТОРОМ МЫ ВСЕ ЖИВЁМ</p>
+        </div>
+        <div class="col-md">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Oy9kQYbl-8U?rel=0" allowfullscreen></iframe>
+            </div>
+            <p>ЗА СПРАВЕДЛИВОСТЬ И ЧЕСТНОСТЬ В ОБЩЕСТВЕ</p>
+        </div>
+    </div>
+
+</div>
 
 </body>
 </html>
--- views/register.php
+++ views/register.php
@@ -5,9 +5,47 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/template/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/template/css/main.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-<a href="/">login</a>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-fluid" src="/images/4.jpg">
+            </div>
+            <div class="col-md">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="/images/4.jpg">
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="/images/4.jpg">
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="/images/4.jpg">
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="/images/4.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md">
+                <img class="img-fluid" src="/images/4.jpg">
+            </div>
+            <div class="col-md">
+                <img class="img-fluid" src="/images/4.jpg">
+            </div>
+            <div class="col-md">
+                <img class="img-fluid" src="/images/4.jpg">
+            </div>
+            <div class="col-md">
+                <img class="img-fluid" src="/images/4.jpg">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
