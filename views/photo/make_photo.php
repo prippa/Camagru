7c75477256aea1cee98b81dd2cd73701c716aa35
prippa <pavelrippa@gmail.com>
2019-10-01 14:59:49 +0300

    make photo front ...

--- views/photo/make_photo.php
+++ views/photo/make_photo.php
@@ -1,17 +1,37 @@
 <div class="container">
 
     <div class="row">
-        <div class="col">
-            <?php require 'views/includes/success_message.php' ?>
+        <div class="col-lg-9">
+            <main>
+                <?php require 'views/includes/success_message.php' ?>
 
-            <form action="/make_photo" method="post" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="user_file">Pick a photo from your device</label>
-                    <input name="file[]" type="file" class="form-control-file"
-                           id="user_file" accept="image/*" tabindex="1" required multiple>
+                <form action="/make_photo" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="user_file">Pick a photo from your device</label>
+                        <input name="file[]" type="file" class="form-control-file"
+                               id="user_file" accept="image/*" tabindex="1" required multiple>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block" tabindex="2">Make</button>
+                </form>
+            </main>
+        </div>
+        <div class="col-lg-3">
+            <aside>
+                <div class="row">
+                    <div class="col-12 pb-3">
+                        <img class="img-fluid" src="/uploads/5d5e9aa02ac92.png" alt="">
+                    </div>
+                    <div class="col-12 pb-3">
+                        <img class="img-fluid" src="/uploads/5d5edd19a1f8a.jpg" alt="">
+                    </div>
+                    <div class="col-12 pb-3">
+                        <img class="img-fluid" src="/uploads/5d5ffd8d17a2d.png" alt="">
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary btn-block" tabindex="3">Upload</button>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary btn-block" tabindex="2">Upload</button>
-            </form>
+            </aside>
         </div>
     </div>
 
