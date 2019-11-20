05fb0f4259d87249242c98898b3bea4d726df40a
prippa <pavelrippa@gmail.com>
2019-11-20 13:51:47 +0200

    change send Mail

--- components/lib/Mail.php
+++ components/lib/Mail.php
@@ -16,23 +16,8 @@ abstract class Mail
      */
     private static function send(string $to, string $subject, string $message): void
     {
-        $encoding = "utf-8";
-
-        // Set preferences for Subject field
-        $subject_preferences = array(
-            "input-charset" => $encoding,
-            "output-charset" => $encoding,
-            "line-length" => 76,
-            "line-break-chars" => "\r\n"
-        );
-
-        // Set mail header
-        $header = "Content-type: text/html; charset=" . $encoding . " \r\n";
-        $header .= "From: <prippa@student.42.fr> \r\n";
-        $header .= "MIME-Version: 1.0 \r\n";
-        $header .= "Content-Transfer-Encoding: 8bit \r\n";
-        $header .= "Date: " . date("r (T)") . " \r\n";
-        $header .= iconv_mime_encode("Subject", $subject, $subject_preferences);
+        $header = "From: The Sender Name <{$_SERVER['HTTP_HOST']}>\r\n";
+        $header .= "Content-type: text/html; charset=utf-8\r\n";
 
         // Send mail
         mail($to, $subject, $message, $header);
